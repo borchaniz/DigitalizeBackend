e3eb472c954808a7a32b98546e8a325c53f2153d
Nadhem <Nadhem@DESKTOP-FG34P78>
2017-08-26 16:53:44 +0100

    all files committed

---
+++ proj/src/AppBundle/Entity/Recruit.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Recruit
+ */
+class Recruit
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $fname;
+
+    /**
+     * @var int
+     */
+    private $phone;
+
+    /**
+     * @var \DateTime
+     */
+    private $birth;
+
+    /**
+     * @var string
+     */
+    private $cv;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Recruit
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Recruit
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set fname
+     *
+     * @param string $fname
+     *
+     * @return Recruit
+     */
+    public function setFname($fname)
+    {
+        $this->fname = $fname;
+
+        return $this;
+    }
+
+    /**
+     * Get fname
+     *
+     * @return string
+     */
+    public function getFname()
+    {
+        return $this->fname;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param integer $phone
+     *
+     * @return Recruit
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return int
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set birth
+     *
+     * @param \DateTime $birth
+     *
+     * @return Recruit
+     */
+    public function setBirth($birth)
+    {
+        $this->birth = $birth;
+
+        return $this;
+    }
+
+    /**
+     * Get birth
+     *
+     * @return \DateTime
+     */
+    public function getBirth()
+    {
+        return $this->birth;
+    }
+
+    /**
+     * Set cv
+     *
+     * @param string $cv
+     *
+     * @return Recruit
+     */
+    public function setCv($cv)
+    {
+        $this->cv = $cv;
+
+        return $this;
+    }
+
+    /**
+     * Get cv
+     *
+     * @return string
+     */
+    public function getCv()
+    {
+        return $this->cv;
+    }
+}
+
